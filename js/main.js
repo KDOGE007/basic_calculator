38da02ae60fd0ff82341cacf2ac3d85dd4c68550
KDOGE007 <kharnthorn@gmail.com>
2020-09-23 20:42:58 +1200

    operator_funtions_added

--- js/main.js
+++ js/main.js
@@ -5,15 +5,71 @@ const calculator = {
   operator: null,
 };
 function inputDigit(digit) {
-  const { displayValue } = calculator;
+  const { displayValue, waitingForSecondOperand } = calculator;
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  }
   //Overwrite displayValue if the current value is 0, otherwise apend the element
-  calculator.displayValue = displayValue === "0" ? digit : displayValue + digit;
+  else {
+    calculator.displayValue =
+      displayValue === "0" ? digit : displayValue + digit;
+  }
+  console.log(calculator);
+}
+
+function inputDecimal(dot) {
+  //fix appending dot to the first operand after pressing the operator key
+  if (calculator.waitingForSecondOperand === true) {
+    calculator.displayValue = "0.";
+    calculator.waitingForSecondOperand = false;
+  }
+  //if the displayValue does not conatain a decimal point
+  if (!calculator.displayValue.includes(dot)) {
+    //concat the decimal point
+    calculator.displayValue += dot;
+  }
+}
+function handleOperator(nextOperator) {
+  //Destructure on the calculator object
+  const { firstOperand, displayValue, operator } = calculator;
+  //parseFloat to convert to number
+  const inputValue = parseFloat(displayValue);
+
+  //verify that firstOperand is null and the inputValue is not a Nan
+  if (firstOperand === null && !isNaN(inputValue)) {
+    calculator.firstOperand = inputValue;
+    //if operator property has been assignd an operator then do a calculation.
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator);
+
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+  console.log(calculator);
+}
+function calculate(firstOperand, secondOperand, operator) {
+  switch (operator) {
+    case "+":
+      return firstOperand + secondOperand;
+    case "-":
+      return firstOperand - secondOperand;
+    case "*":
+      return firstOperand * secondOperand;
+    case "/":
+      return firstOperand / secondOperand;
+    default:
+      return secondOperand;
+  }
 }
 
 function updateDisplay() {
   //select the element with id of 'result'//
   const display = document.querySelector("#result");
-  //update the element with the object 'calculator' displayValue property//
+  //update the element with the calculator's displayValue property//
   display.innerText = calculator.displayValue;
 }
 
@@ -28,11 +84,13 @@ keys.addEventListener("click", (event) => {
     return;
   }
   if (target.classList.contains("buttonOperator")) {
-    console.log("buttonOperator", target.value);
+    handleOperator(target.value);
+    updateDisplay();
     return;
   }
   if (target.classList.contains("buttonDecimal")) {
-    console.log("buttonDecimal", target.value);
+    inputDecimal(target.value);
+    updateDisplay();
     return;
   }
   if (target.classList.contains("buttonClear")) {
